61aabaa9bea811c72b22fd18f4f03134b869bdc2
yuxuanjava <han630389657@vip.qq.com>
2019-12-16 11:13:54 +0800

    修改注释

--- src/main/java/club/yuxuan/yun/xuans/designmode/decorator/Decorator.java
+++ src/main/java/club/yuxuan/yun/xuans/designmode/decorator/Decorator.java
@@ -1,13 +1,5 @@
 package club.yuxuan.yun.xuans.designmode.decorator;
 
-/**
- * 装饰模式
- *      动态地给一个对象添加一些额外的职责
- *      就增加功能来说，装饰模式比生成子类更为灵活。
- *
- * @author yuxuan.han
- * @date 2019/12/8 20:25
- **/
 public abstract class Decorator extends Component {
     
     protected Component component;
