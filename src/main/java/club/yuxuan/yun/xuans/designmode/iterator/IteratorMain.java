8b97158606356753afb727bc8ecfc2791aedc37b
yuxuan_java <han630389657@vip.qq.com>
2019-12-23 23:45:19 +0800

    IteratorMain

---
+++ src/main/java/club/yuxuan/yun/xuans/designmode/iterator/IteratorMain.java
@@ -0,0 +1,23 @@
+package club.yuxuan.yun.xuans.designmode.iterator;
+
+/**
+ * 迭代器模式
+ *      提供一种方法顺序访问同一个聚合对象中各个元素，而又不暴露该对象的内部表示。
+ *      当需要访问一个聚焦对象，而且不管这些对象是什么都需要遍历的时候，就应该考虑使用迭代器模式
+ *      或需要对聚焦有多种方式遍历时，可以考虑用迭代器模式
+ *      为遍历不同的聚焦结构提供如开始、下一个、是否结束、当前是哪一项等统一的接口。
+ *      
+ *      
+ *
+ * @author yuxuan.han
+ * @date 2019/12/23 23:04
+ **/
+public class IteratorMain {
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("------done------");
+    }
+    
+}
