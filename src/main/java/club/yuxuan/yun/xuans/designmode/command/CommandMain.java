cbdfa29c035426328ad358c86b70fa6d5d352209
yuxuan_java <han630389657@vip.qq.com>
2019-12-25 00:02:33 +0800

    CommandMain

---
+++ src/main/java/club/yuxuan/yun/xuans/designmode/command/CommandMain.java
@@ -0,0 +1,19 @@
+package club.yuxuan.yun.xuans.designmode.command;
+
+/**
+ * 命令模式
+ *      将一个请求封装为一个对象，从而使你可用不同的请求对客户进行参数化；
+ *      对请求排队或记录错误日志，以及支持可撤销的操作。
+ *
+ * @author yuxuan.han
+ * @date 2019/12/25 00:01
+ **/
+public class CommandMain {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("------done------");
+    }
+    
+}
